c6a4d1d9cdc4083510bf83a25e12510a0bf71616
陈安东 <chenandong@Andongs-MacBook-Pro.local>
2024-10-14 00:21:21 +0800

    feat: meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,17 +8,12 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: '2cai',
   description: 'some utils',
+  viewport: 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
